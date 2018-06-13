01dfa3ce8f952a677e92f424989f5d7b6eefd7c2
Eve Jones <evejones@calvisitor-10-105-150-106.calvisitor.1918.berkeley.edu>
2018-06-12 21:17:14 -0700

    Set up api routes file

---
+++ controllers/api-routes.js
@@ -0,0 +1,38 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/pets", function(req, res) {
+    //This will be used if we have a webpage that shows all the pets in the database
+    db.Pets.findAll({}).then(function(dbPets) {
+      res.json(dbPets);
+    });
+  });
+
+  app.get("/api/pets/:id", function(req, res) {
+    //Find one particular pet in the database
+    db.Pets.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbPets) {
+      res.json(dbPets);
+    });
+  });
+
+  app.post("/api/pets", function(req, res) {
+    db.Pets.create(req.body).then(function(dbPets) {
+      res.json(dbPets);
+    });
+  });
+
+  app.delete("/api/authors/:id", function(req, res) {
+    db.Pets.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbPets) {
+      res.json(dbPets);
+    });
+  });
+
+};
