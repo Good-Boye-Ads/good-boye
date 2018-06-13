a22a40f514ec8fb23d294101d2e96d9f941dd734
Sunny <sundeepkmanik@gmail.com>
2018-06-12 21:15:39 -0700

    server????

---
+++ server.js
@@ -0,0 +1,33 @@
+// DEPENDENCIES
+// ===================
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// SETS UP EXPRESS
+// ===================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var db = require("./models");
+
+// SETS UP EXPRESS TO HANDLE DATA PARSING
+// ===================
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
+// static directory
+app.use(express.static("public"));
+
+// ROUTES
+// ===================
+require("./controllers/api-routes.js")(app);
+require("./controlers/html-routes.js")(app);
+
+// SYNC
+// ===================
+db.sequelize.sync({}).then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    });
+});
