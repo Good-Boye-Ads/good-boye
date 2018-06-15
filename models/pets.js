827b1181a876a599b37389bd9277d8c3b89e75b6
Eve Jones <evejones@reimageds-MacBook-Pro.local>
2018-06-14 18:58:28 -0700

    Debugged

--- models/pets.js
+++ models/pets.js
@@ -2,39 +2,39 @@ module.exports = function(sequelize, DataTypes) {
     var Pets = sequelize.define("Pets", {
       pet_type: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
         validate: {
             len: [1, 25]
         }
       },
       pet_name: {
         type:  DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
         validate: {
             len: [1, 25]
         }
       },
       pet_age: {
-        type: DataTypes.INT,
-        allowNull: false,
+        type: DataTypes.INTEGER,
+        allowNull: true,
       },
       location: {
         type:  DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
         validate: {
             len: [1, 50]
         }
       },
       image_url: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
         validate: {
             len: [1, 300]
         }
       },
       url: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
         validate: {
             len: [1, 300]
         }
