b370f4e4a2aa12477189a1b5d31db9d5221eb2c1
Eve Jones <evejones@reimageds-MacBook-Pro.local>
2018-06-16 10:30:50 -0700

    Added an html route

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -6,5 +6,8 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
+  app.get("/add-a-pet", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/add.html"));
+  });
 
 };
